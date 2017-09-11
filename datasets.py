543ad40a6524c6dbcee8415ee87f107805d9d267
Pavel Izmailov <izmailovpavel@gmail.com>
2017-09-10 21:11:23 -0400

    Added priors module

--- datasets.py
+++ datasets.py
@@ -1,7 +1,6 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
-
 class Dataset:
 	"""
 	Abstract base class representing a dataset and defyning the interface.
---
+++ priors.py
@@ -0,0 +1,34 @@
+import torch
+from abc import ABCMeta, abstractmethod
+
+class Prior:
+    """
+    Abstract class representing prior distribution over NN weights.
+    """
+    __metaclass__ = ABCMeta
+    
+    @abstractmethod
+    def log_density(self, model):
+        """
+        Computes prior log-density of the `model`.
+		Args:
+			model: torch.nn.Module instance
+        """
+        pass
+
+class FactorizedNormalPrior(Prior):
+    
+    def __init__(self, std=1.0):
+        """
+		Factorized Normal prior over the parameters of the model.
+		
+	    Args:
+            std: standard diviation of the normal prior
+		"""
+        self.std = std
+    
+    def log_density(self, model):
+        log_prior = 0.
+        for param in model.parameters():
+            log_prior += torch.sum(param / self.std)**2
+        return log_prior
