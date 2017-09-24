f70ed5ac06d2358211b0391287a8c7648d55206a
Pavel Izmailov <izmailovpavel@gmail.com>
2017-09-24 12:14:48 -0400

    Retabbed

--- bgan.py
+++ bgan.py
@@ -7,8 +7,8 @@ class BGAN:
                  generator_prior, discriminator_prior, num_data,
                  J=1, M=1, eta=2e-4, alpha=0.01, observed_gen=50):
         """
-		Creates a Bayesian GAN for the given generator and discriminator.
-		
+        Creates a Bayesian GAN for the given generator and discriminator.
+        
         Args:
             generator: `torch.nn.Module` instance, generator network
             discriminator: `torch.nn.Module` instance, discriminator network
@@ -21,8 +21,8 @@ class BGAN:
             eta: float, learning rate; also affects noise variance in SGHMC
             alpha: float, momentum variable; also affects noise variance in 
                 SGHMC
-			observed_gen: number of data observed by the generator; this 
-				hyper-parameter affects the uncertainty in the generator weights
+            observed_gen: number of data observed by the generator; this 
+                hyper-parameter affects the uncertainty in the generator weights
         """
         super(BGAN, self).__init__()
         
@@ -45,8 +45,8 @@ class BGAN:
         self._init_optimizers()
             
     def loss(self, x_batch):
-		"""
-		Computes the losses for the biven batch of data samples.
+        """
+        Computes the losses for the biven batch of data samples.
 
         Args:
             x_batch: `torch.FloatTensor` of shape `(batch_size, x_dim)`; data
@@ -55,7 +55,7 @@ class BGAN:
         Returns:
             d_loss: float, discriminator loss
             g_loss: float, generator loss
-		"""
+        """
         batch_size = x_batch.size()[0]
         x_fake = self.sample(batch_size)
         x_real = x_batch
@@ -76,21 +76,21 @@ class BGAN:
         #discriminator loss
         d_loss = -(bce_real + bce_fake) * self.eta 
 #         d_loss += (self.discriminator_prior.log_density(self.discriminator) 
-#						* self.eta #/ self.num_data)
+#                       * self.eta #/ self.num_data)
 #         d_loss += self.noise(self.generator, noise_std) #/ self.num_data
         d_loss *= -1.
         
         #generator loss
         g_loss = bce_fake * self.eta
         g_loss += (self.generator_prior.log_density(self.generator) 
-					* self.eta / self.obsered_gen)
+                    * self.eta / self.obsered_gen)
         g_loss += self.noise(self.generator, noise_std) / self.observed_gen
         g_loss *= -1.
         return d_loss, g_loss
         
     @staticmethod
     def noise(model, std):
-		"""
+        """
         Multiplies all the variables by a normal rv for SGHMC.
 
         Args:
@@ -106,16 +106,16 @@ class BGAN:
         return loss
     
     def _init_optimizers(self):
-		"""
+        """
         Initializes the optimizers for BGAN.
         """
         self.d_optimizer = optim.SGD(self.discriminator.parameters(), lr=1, 
-							momentum=(1 - self.alpha))
+                            momentum=(1 - self.alpha))
         self.g_optimizer = optim.SGD(self.generator.parameters(), lr=1, 
-							momentum=(1 - self.alpha))
+                            momentum=(1 - self.alpha))
         
     def sample(self, n_samples=1):
-		"""
+        """
         Samples from BGAN generator.
 
         Args:
@@ -130,7 +130,7 @@ class BGAN:
         
     
     def step(self, x_batch):
-		"""
+        """
         Makes an SGHMC step for the parameters of BGAN. 
 
         Args:
