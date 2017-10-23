d475ec0e2eb2e3952e022168f9d6664060249531
Pavel Izmailov <izmailovpavel@gmail.com>
2017-10-23 10:40:39 -0400

    Fix connection script

--- connect_ipython.sh
+++ connect_ipython.sh
@@ -16,7 +16,7 @@ while [ "$1" != "" ]; do
                                 port_local=$1
                                 ;;
         -pr | --port_remote )   shift
-                                port_local=$1
+                                port_remote=$1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -25,7 +25,7 @@ while [ "$1" != "" ]; do
     shift
 done
 
-echo "Connecting to nikola0$machine"
+echo "Connecting to nikola0$machine at port $port_remote to port $port_local"
 
 lsof -ti:$port_local | xargs kill -9
 ssh -N -f -L localhost:$port_local:localhost:$port_remote pi49@nikola-compute0$machine.coecis.cornell.edu 
