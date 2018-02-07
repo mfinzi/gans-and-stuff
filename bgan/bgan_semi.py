ba2c411389993198fab2f798824d757ee597c1ca
Marc Anton Finzi <maf388@en-cs-nikola-compute01.coecis.cornell.edu>
2018-02-07 18:40:30 -0500

    testing BGANS with unlabeled SVNH

--- bgan/bgan_semi.py
+++ bgan/bgan_semi.py
@@ -4,21 +4,58 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 import torchvision.utils as tutils
+import os
 
+def logOneMinusSoftmax(x):
+    """ numerically more stable version of log(1-softmax(x)) """
+    max_vals, _ = torch.max(x, 1)
+    shifted_x = x - max_vals.unsqueeze(-1).expand_as(x)
+    exp_x = torch.exp(shifted_x)
+    sum_exp_x = exp_x.sum(1).unsqueeze(-1).expand_as(exp_x)
+    first = torch.log(sum_exp_x - exp_x)
+    second = torch.log(sum_exp_x)
+    return first - second
 
-class BganS:
+class BGANS:
     
-    def __init__(self, G, D, dataloaders, save_dir, MAP = False, opt_params=(2e-4,0.01)):
+    def __init__(self, G, D, dataloaders, save_dir, MAP = True, 
+                            eta=2e-4, alpha=.01):
         """ Creates a Bayesian GAN for generator G, discriminator D, and optimizer O.
         """
         self.G = G.cuda()
-        self.D = D.cuda()
-        (self.lab_trainloader,self.unl_trainloader,self.devloader,
-                self.testloader) = dataloaders
+        self.D = D.cuda() #Assert self.D.K > 1
+
+        (self.unl_train, self.lab_train, self.dev, self.test) = dataloaders
+        self._setMode() # self.mod = {"uns","semi","fully"}
+
         self.MAP = MAP
-        self._init_optimizers(opt_params)
-            
-    def loss(self, x_unlab, x_lab = None, y_lab = None):
+        self.save_dir = save_dir
+        if not os.path.exists(save_dir): os.makedirs(save_dir)
+        
+        self.eta, self.alpha = eta, alpha
+        self._initOptimizers()
+
+    def _setMode(self):
+        if self.unl_train!=None and self.lab_train!=None: self.mode = "semi"
+        elif self.unl_train!=None: self.mode = "uns"
+        elif self.lab_train!=None: self.mode = "fully"
+        else: raise Exception("No viable dataloaders")
+        
+
+    def _initOptimizers(self):
+        """
+        Initializes the optimizers for BGAN.
+        """
+        if self.MAP:
+            self.g_optimizer = optim.Adam(self.G.parameters(), 
+                lr=self.eta, betas=(0.5, 0.999))
+        else: self.g_optimizer = optim.Adam(self.G.parameters(), 
+                lr=self.eta, betas=(1-self.alpha, 0.999))
+
+        self.d_optimizer = optim.Adam(self.D.parameters(),
+            lr=self.eta, betas=(0.5, 0.999))
+
+    def loss(self, x_unlab = None, x_lab = None, y_lab = None):
         """
         Computes the losses for the biven batch of data samples.
 
@@ -36,43 +73,37 @@ class BganS:
         """
         batch_size = x_unlab.size()[0]
         x_fake = self.sample(batch_size).cuda()
-        #x_lab = x_lab.cuda()
-        #if x_unlab: x_unlab = x_unlab.cuda()
-
-        # Generated samples are labeled as fake (last label)
-        y_fake = torch.ones(batch_size)*self.D.K_classes
-        #y_fake = y_fake.cuda()
-
-        # Binary classification, real labels are 0's, fake are 1
-        if not y_lab: y_lab = torch.zeros(batch_size)
-        y_lab = y_lab.cuda()
         
-            
-        cross_entropy = nn.CrossEntropyLoss()
-        fake_loss = cross_entropy(self.D(x_fake.detach()), y_fake) #detach for speed
-        unl_loss = -1*cross_entropy(self.D(x_unlab),y_fake)
+
+        logSoftMax = nn.LogSoftmax()
+
+        # Losses for generated samples are -log P(y=K+1|x)
+        fake_losses = -1*logSoftMax(self.D(x_fake))[:,self.D.K]
         
-        if y_lab: lab_loss = cross_entropy(self.D(x_lab), y_lab)
-        if x_unlab: # Not strictly Kosher, need to test
-            
-        else: 
-            unl_loss = 0
+        if self.mode!="fully":# Losses for unlabeled samples are -log(1-P(y=K+1|x))
+            unl_losses =  -1*logOneMinusSoftmax(self.D(x_unlab))[:,self.D.K]
+        else: unl_losses = 0
+
+        if self.mode!="uns": # Losses for labeled samples are -log(P(y=yi|x))
+            lab_losses = -1*logSoftMax(self.D(x_lab))[:,y_lab]
+        else: lab_losses = 0
 
         #discriminator loss
-        d_loss = fake_loss + unl_loss + lab_loss 
-        
-        #generator loss
-        g_loss = -1*fake_loss
+        d_loss = torch.sum(fake_losses + unl_losses + lab_losses)/batch_size
+        #generator loss (original GAN loss, NOT non-saturating)
+        g_loss = -1*torch.sum(fake_losses)/batch_size
 
         if not self.MAP:
-            noise_std = np.sqrt(2 * self.O.alpha * self.O.eta)
-            g_loss += (self.noise_loss(self.generator, noise_std) / 
-                    (self.O.gn * self.O.eta))
+            observed_gen = 20000 #batch_size # 50 # What is this hyperparameter doing??
+            # It seems like it should be fixed to batch_size
+            noise_std = np.sqrt(2 * self.alpha * self.eta)
+            g_loss += (self.noiseLoss(self.G, noise_std) / 
+                    (observed_gen * self.eta))
 
         return d_loss, g_loss
         
     @staticmethod
-    def noise_loss(model, std):
+    def noiseLoss(model, std):
         """
         Multiplies all the variables by a normal rv for SGHMC.
 
@@ -91,22 +122,6 @@ class BganS:
             loss += torch.sum(n * param)
         return loss
     
-    def _init_optimizers(opt_params):
-        """
-        Initializes the optimizers for BGAN.
-        """
-        eta, alpha = opt_params
-        if self.MAP:
-            self.g_optimizer = optim.Adam(self.G.parameters(), 
-                lr=eta, betas=(0.99, 0.999))
-        else self.g_optimizer = optim.Adam(self.G.parameters(), 
-                lr=eta, betas=(1-alpha, 0.999))
-
-        self.d_optimizer = optim.Adam(self.D.parameters(),
-            lr=eta, betas=(0.99, 0.999))
-        
-        
-        
     def sample(self, n_samples=1):
         """
         Samples from BGAN generator.
@@ -117,12 +132,11 @@ class BganS:
             `torch` variable containing `torch.FloatTensor` of shape 
                 `(n_samles, z_dim)`
         """
-        z = torch.randn(n_samples, self.z_dim)
+        z = torch.randn(n_samples, self.G.z_dim)
         zv = Variable(z.cuda())
-        return self.generator(zv)
+        return self.G(zv)
         
-    
-    def step(self, x_real, y_real = None, x_unlab = None):
+    def step(self, x_real = None, y_real = None, x_unlab = None):
         """
         Makes an SGHMC step for the parameters of BGAN. 
 
@@ -130,28 +144,22 @@ class BganS:
             x_batch: `torch.FloatTensor` of shape `(batch_size, x_dim)`; data
                 samples
         """
-
-        batchv = Variable(x_batch)
-        self.discriminator.zero_grad()
-        self.generator.zero_grad()
+        self.D.zero_grad()
+        self.G.zero_grad()
         d_loss, g_loss = self.loss(x_real,y_real,x_unlab)
         d_loss.backward(retain_graph=True)
         self.d_optimizer.step()
         
-#        self.generator.zero_grad()
-#        self.discriminator.zero_grad()
-#        
-#        d_loss, g_loss = self.loss(batchv)
         g_loss.backward()
         self.g_optimizer.step()        
         
     def train(self, epochs=100):
-        if self.lab_trainloader:
-            self.semi_train(epochs)
-        else:
-            self.unsupervised_train(epochs)
+        if self.mode == "uns":
+            self.unsupervisedTrain(epochs)
+        else: raise Exception('Semisupervised and supervised modes not yet written')
     
-    def semi_train(self, epochs):
+    def semiTrain(self, epochs):
+        print("semisupervised?")
         return
         # labeled_iterator = iter(self.lab_trainloader)
         # dev_iterator = iter(self.devloader)
@@ -164,24 +172,26 @@ class BganS:
 
         #         self.step(x_unlabeled,x_lab,ylab)
 
-    def unsupervised_train(self, epochs):
+    def unsupervisedTrain(self, epochs):
         losses = np.empty((0,2)) # losses = [Dloss,Gloss]
         for epoch in range(epochs):
-            for i, unlabeled_data in enumerate(self.unl_trainloader):
+            for i, unlabeled_data in enumerate(self.unl_train):
                 x_unlabeled = Variable(unlabeled_data[0].cuda())
                 self.step(x_unlabeled)
 
-                if i%177==0:
-                    batchLosses = self.loss(x_unlabeled).cpu().data.numpy().mean(axis=0)
-                    np.concatenate((losses,batchLosses))
-                    d_loss,g_loss = losses[-1]
-                    print('[%d/%d][%d/%d] Loss_D: %.4f \
-                    Loss_G: %.4f %(epoch,epochs,i,len(self.unl_trainloader),d_loss,g_loss))
+                if i%101==0:
+                    getMean = lambda ell: ell.cpu().data.numpy().mean(axis=0)
+                    batchLosses = np.array(list(map(getMean, self.loss(x_unlabeled)))).reshape(1,2)
+                    losses = np.concatenate((losses,batchLosses))
+
+                if i%500==0:
+                    d_loss,g_loss = losses[-1]*.5 + losses[-2]*.25 + losses[-3]*.25 
+                    print("[%2d/%d][%3d/%d] Loss_D: %.4f \
+                    Loss_G: %.4f" %(epoch,epochs,i,len(self.unl_train),d_loss,g_loss))
 
             if epoch%5==0:
-                self.sample(10)
-                tutils.save_image(self.sample(10).data, '%s/fake_samples_epoch_%03d.png'\
-                            %(save_dir,epoch),normalize=True)
+                tutils.save_image(self.sample(8).data, '%s/fake_samples_epoch_%03d.png'\
+                            %(self.save_dir,epoch),normalize=True)
 
                 
                 
---
+++ bgan/networkparts.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import numpy as np
+
+# weight init is automatically done in the module initialization
+# see https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/conv.py
+def weight_init_he(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, np.sqrt(2. / n))
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+
+class Expression(nn.Module):
+    def __init__(self, func):
+        super(Expression, self).__init__()
+        self.func = func
+        
+    def forward(self, input):
+        return self.func(input)        
+        
+class BadGanG(nn.Module):
+    
+    def __init__(self, d=128, z_dim=100):
+        super(BadGanG, self).__init__()
+        self.z_dim = z_dim
+        self.core_net = nn.Sequential(
+            nn.Linear(z_dim, 4*4*(4*d)), nn.BatchNorm1d(4*4*(4*d)), nn.ReLU(),
+            Expression(lambda tensor: tensor.view(tensor.size(0),4*d,4,4)),
+            nn.ConvTranspose2d(4*d,2*d,5,2,2,1), nn.BatchNorm2d(2*d), nn.ReLU(),
+            nn.ConvTranspose2d(2*d,  d,5,2,2,1), nn.BatchNorm2d(d),   nn.ReLU(),
+            nn.ConvTranspose2d(d  ,  3,5,2,2,1), nn.Tanh(),
+        )
+    def forward(self, z):
+        return self.core_net(z)
+    
+class BadGanD(nn.Module):
+    
+    def __init__(self, d=64, K=1):
+        super(BadGanD, self).__init__()
+        self.K = K # Number of 'supervised' classes
+        self.core_net = nn.Sequential(
+            nn.Conv2d(  3,  d,3,1,1), nn.BatchNorm2d(  d), nn.LeakyReLU(0.2),
+            nn.Conv2d(  d,  d,3,1,1), nn.BatchNorm2d(  d), nn.LeakyReLU(0.2),
+            nn.Conv2d(  d,  d,3,2,1), nn.BatchNorm2d(  d), nn.LeakyReLU(0.2),
+            #nn.Dropout2d(0.5),
+            nn.Conv2d(  d,2*d,3,1,1), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            nn.Conv2d(2*d,2*d,3,1,1), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            nn.Conv2d(2*d,2*d,3,2,1), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            #nn.Dropout2d(0.5),
+            nn.Conv2d(2*d,2*d,3,1,0), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            nn.Conv2d(2*d,2*d,1,1,0), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            nn.Conv2d(2*d,2*d,1,1,0), nn.BatchNorm2d(2*d), nn.LeakyReLU(0.2),
+            Expression(lambda tensor: tensor.mean(3).mean(2).squeeze()),
+            nn.Linear(2*d,self.K+1),
+        )
+    def forward(self, x):
+        return self.core_net(x)
