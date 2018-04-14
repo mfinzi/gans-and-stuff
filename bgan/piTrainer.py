fixed migration error in vatCnnTrainer, added softmax mse loss that takes gradients with respect to both inputs

bgan/piTrainer.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch.nn as nn
 
 from bgan.cnnTrainer import CnnTrainer
-from bgan.losses import softmax_mse_loss
+from bgan.losses import softmax_mse_loss, softmax_mse_loss_both
 from bgan.schedules import sigmoidConsRamp
 
 class PiTrainer(CnnTrainer):
@@ -21,7 +21,7 @@ class PiTrainer(CnnTrainer):
         pred1 = self.CNN(x_unlab)
         pred2 = self.CNN(x_unlab)
         weight = self.hypers['cons_weight']*self.consRamp(self.epoch)
-        cons_loss =  weight*softmax_mse_loss(pred1, pred2)/self.hypers['ul_BS']
+        cons_loss =  weight*softmax_mse_loss_both(pred1, pred2)/self.hypers['ul_BS']
         return cons_loss
 
     def loss(self, *data):

bgan/vatCnnTrainer.py
@@ -80,7 +80,7 @@ class VatCnnTrainer(CnnTrainer):
             x_unlab = trainData[1][0]; someX = x_unlab[:16]
             r_adv = self.hypers['advEps'] * self.getAdvPert(self.CNN, someX)
             adversarialImages = (someX + r_adv).cpu().data
-            imgComparison = torch.cat((adversarialImages, x_unlab[:16]))
+            imgComparison = torch.cat((adversarialImages, someX.cpu().data))
             self.writer.add_image('adversarialInputs', #,range=(-2.5,2.5)
                     vutils.make_grid(imgComparison,normalize=True), step)
             self.metricLog.update({'Unlab_loss(batch)':